d3596c528275894e6123470409d184498bf52e2b
Chris Ohk <utilForever@gmail.com>
2015-09-23 21:50:06 +0900

    Update main.cpp

--- SimpleRPG-Text/main.cpp
+++ SimpleRPG-Text/main.cpp
@@ -1,4 +1,17 @@
+#include "JsonBox.h"
+
+#include "Item.h"
+#include "Armor.h"
+#include "Weapon.h"
+#include "EntityManager.h"
+
+EntityManager entityManager;
+
 int main(int argc, char* argv[])
 {
-	
+	entityManager.LoadJSON<Item>("items.json");
+	entityManager.LoadJSON<Weapon>("weapons.json");
+	entityManager.LoadJSON<Armor>("armor.json");
+
+	return 0;
 }
