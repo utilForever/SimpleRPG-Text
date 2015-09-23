b5b04b81f5e804aa2d001b63b08c8a98cba82181
Chris Ohk <utilForever@gmail.com>
2015-09-23 21:49:51 +0900

    Create Weapon Class

---
+++ SimpleRPG-Text/Weapon.cpp
@@ -0,0 +1,26 @@
+#include <string>
+
+#include "JsonBox.h"
+
+#include "Weapon.h"
+#include "Item.h"
+#include "EntityManager.h"
+
+Weapon::Weapon(std::string _id, std::string _name, std::string _description, int _damage)
+	: Item(_id, _name, _description), damage(_damage)
+{
+
+}
+
+Weapon::Weapon(std::string _id, JsonBox::Value& _v, EntityManager* _manager)
+	: Item(_id, _v, _manager)
+{
+	Load(_v, _manager);
+}
+
+void Weapon::Load(JsonBox::Value& v, EntityManager* manager)
+{
+	JsonBox::Object obj = v.getObject();
+
+	damage = obj["weapon"].getInteger();
+}
---
+++ SimpleRPG-Text/Weapon.h
@@ -0,0 +1,24 @@
+#ifndef WEAPON_H
+#define WEAPON_H
+
+#include <string>
+
+#include "JsonBox.h"
+
+#include "Item.h"
+
+class EntityManager;
+
+class Weapon : public Item
+{
+private:
+	int damage;
+
+public:
+	Weapon(std::string _id, std::string _name, std::string _description, int _damage);
+	Weapon(std::string _id, JsonBox::Value& _v, EntityManager* _manager);
+
+	void Load(JsonBox::Value& v, EntityManager* manager) override;
+};
+
+#endif
