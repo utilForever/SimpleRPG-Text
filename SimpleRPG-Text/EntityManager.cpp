77694227e72eb9a4db04c4fb1775f89f3111d631
Chris Ohk <utilForever@gmail.com>
2015-09-30 08:12:22 +0900

    Modify uppercase to lowercase

--- SimpleRPG-Text/EntityManager.cpp
+++ SimpleRPG-Text/EntityManager.cpp
@@ -5,6 +5,9 @@
 #include "Item.h"
 #include "Weapon.h"
 #include "Armor.h"
+#include "Creature.h"
+#include "Area.h"
+#include "Door.h"
 
 EntityManager::EntityManager()
 {
@@ -30,7 +33,7 @@ void EntityManager::LoadJSON(std::string fileName)
 	v.loadFromFile(fileName);
 
 	JsonBox::Object obj = v.getObject();
-	for (auto& entity : obj)
+	for (auto entity : obj)
 	{
 		std::string key = entity.first;
 		data[key] = dynamic_cast<Entity*>(new T(key, entity.second, this));
@@ -46,14 +49,23 @@ T* EntityManager::GetEntity(std::string id)
 		return nullptr;
 }
 
-template <> std::string EntityToString<Item>() { return "Item"; }
-template <> std::string EntityToString<Weapon>() { return "Weapon"; }
-template <> std::string EntityToString<Armor>() { return "Armor"; }
+template <> std::string EntityToString<Item>() { return "item"; }
+template <> std::string EntityToString<Weapon>() { return "weapon"; }
+template <> std::string EntityToString<Armor>() { return "armor"; }
+template <> std::string EntityToString<Creature>() { return "creature"; }
+template <> std::string EntityToString<Area>() { return "area"; }
+template <> std::string EntityToString<Door>() { return "door"; }
 
 template void EntityManager::LoadJSON<Item>(std::string);
 template void EntityManager::LoadJSON<Weapon>(std::string);
 template void EntityManager::LoadJSON<Armor>(std::string);
+template void EntityManager::LoadJSON<Creature>(std::string);
+template void EntityManager::LoadJSON<Area>(std::string);
+template void EntityManager::LoadJSON<Door>(std::string);
 
 template Item* EntityManager::GetEntity<Item>(std::string);
 template Weapon* EntityManager::GetEntity<Weapon>(std::string);
 template Armor* EntityManager::GetEntity<Armor>(std::string);
+template Creature* EntityManager::GetEntity<Creature>(std::string);
+template Area* EntityManager::GetEntity<Area>(std::string);
+template Door* EntityManager::GetEntity<Door>(std::string);
